61251f18387aaefd5d6b1714944e8348dd50f63f
Nicholas Mullen <nwmullen@gmail.com>
2020-06-05 23:59:36 -0400

    mvp

---
+++ auth/auth-router.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department
+  };
+
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username }).first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials '});
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
